cd4257b860628f0f0eb16737feab651533465b65
m4rxhs-cyber <m4rxhs3301@gmail.com>
2021-10-27 12:57:13 -0300

    Crypter v1.1

--- Crypter/crypter.c
+++ Crypter/crypter.c
@@ -2,57 +2,65 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-        char msg[9999];
-        char crip_msg[9999];
+        char msg[9999], crip_msg[9999], resposta='s';
         int tamanho, x, menu;
 
-        //banner
-        printf(" ######  ########  ##    ## ########  ######## ######## ########\n");
-        printf("##    ## ##     ##  ##  ##  ##     ##    ##    ##       ##     ## \n");
-        printf("##       ##     ##   ####   ##     ##    ##    ##       ##     ## \n");
-        printf("##       ########     ##    ########     ##    ######   ######## \n");
-        printf("##       ##   ##      ##    ##           ##    ##       ##   ##\n");
-        printf("##    ## ##    ##     ##    ##           ##    ##       ##    ##\n");
-        printf(" ######  ##     ##    ##    ##           ##    ######## ##     ## \n");
-        printf("                       made by m4rxhs_cyber                             \n");
-        //menu
-        printf("\n |Qual a operacao voce deseja?\n");
-        printf("\n+---------------------------------+\n");
-        printf("| 1 - Cryptografar                |\n");
-        printf("| 2 - Descryptografar             |");
-        printf("\n+---------------------------------+\n");
-        printf("--> ");
-        scanf("%d", &menu);
-
-        //encrypt
-        if (menu == 1){
-        printf(" |Digite texto ou palavra a ser criptografada: \n");
-        fflush(stdin);
-        fgets(msg,9998,stdin);
-        tamanho = strlen(msg);
-
-        for(x=0;x<tamanho;x++)
+        while(resposta != 'n')
         {
-                crip_msg[x] = msg[x] + 3;
-        }
+            //banner
+            printf(" ######  ########  ##    ## ########  ######## ######## ########\n");
+            printf("##    ## ##     ##  ##  ##  ##     ##    ##    ##       ##     ## \n");
+            printf("##       ##     ##   ####   ##     ##    ##    ##       ##     ## \n");
+            printf("##       ########     ##    ########     ##    ######   ######## \n");
+            printf("##       ##   ##      ##    ##           ##    ##       ##   ##\n");
+            printf("##    ## ##    ##     ##    ##           ##    ##       ##    ##\n");
+            printf(" ######  ##     ##    ##    ##           ##    ######## ##     ## \n");
+            printf("                       made by m4rxhs_cyber                             \n");
+            //menu
+            printf("\n |Qual a operacao voce deseja?\n");
+            printf("\n+---------------------------------+\n");
+            printf("| 1 - Cryptografar                |\n");
+            printf("| 2 - Descryptografar             |");
+            printf("\n+---------------------------------+\n");
+            printf("--> ");
+            scanf("%d", &menu);
 
-        printf(" |A mensagem criptografada e : %s\n\n", crip_msg);
-        }
+            //encrypt
+            if (menu == 1){
+            printf(" |Digite texto ou palavra a ser criptografada: \n");
+            printf("--> ");
+            fflush(stdin);
+            fgets(msg,9998,stdin);
+            tamanho = strlen(msg);
 
-        //decrypt
-        if (menu ==2){
-        printf(" |Digite mensagem a ser descriptografada: \n");
-        fflush(stdin);
-        fgets(crip_msg,9998,stdin);
-        tamanho = strlen(crip_msg);
+            for(x=0;x<tamanho;x++)
+            {
+                    crip_msg[x] = msg[x] + 3;
+            }
+                printf(" |A mensagem criptografada e : %s\n\n", crip_msg);
+            }
 
-        for(x=0;x<tamanho;x++)
-        {
-                msg[x] = crip_msg[x] - 3;
+            //decrypt
+            if (menu ==2){
+            printf(" |Digite mensagem a ser descriptografada: \n");
+            printf("--> ");
+            fflush(stdin);
+            fgets(crip_msg,9998,stdin);
+            tamanho = strlen(crip_msg);
+
+            for(x=0;x<tamanho;x++)
+            {
+                    msg[x] = crip_msg[x] - 3;
+            }
+                printf(" |A mensagem descriptografada e : %s\n\n", msg);
+            }
+
+            printf("\n |Deseja fazer mais uma operacao?[s/n]: \n");
+            printf("--> ");
+            fflush(stdin);
+            scanf(" %c", &resposta);
+            system("cls");
         }
-            printf(" |A mensagem descriptografada e : %s\n\n", msg);
-        }
-        return 0;
 }
